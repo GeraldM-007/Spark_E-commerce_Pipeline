7038cca5f213faa73008bb4ba8a2d30d0268b718
GeraldM-007 <geraldmuchuku237@gmail.com>
2026-06-01 23:39:54 +0300

    aggregations and window functions

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
+from pyspark.sql.window import Window
 
 
 spark = SparkSession.builder.appName("e-commerce").getOrCreate()
@@ -54,7 +55,7 @@ original_customers_df.show(truncate=False)
 original_order_items_df.show()
 original_orders_df.show()
 original_returns_df.show(truncate=False)
-print('Completed printing the Original\n')
+print('Completed printing the Original DFs\n')
 
 
 #DATA QUALITY AND CLEANING
@@ -89,9 +90,9 @@ cleaned_order_items_df = order_items_df.na.drop(subset=["order_id"])
 #Handling NULLs gracefully
 rejected_orders_df = orders_df.filter(col('customer_id').isNull() | col('order_id').isNull())
 
-print('Rejected')
+print('Rejected DFs Start')
 rejected_orders_df.show()
-print("Rejected End\n")
+print("Rejected DFs End\n")
 
     
 #CUSTOMER_TIER TO LOWER_CASE
@@ -100,8 +101,8 @@ cleaned_customers_df = cleaned_customers_df.withColumn(
         lower(customers_df['customer_tier'])
     )
     
-    #DATE NORMALISATION
-    #date normalisation expr() that allows writing SQL-like expressions inside spark
+#DATE NORMALISATION
+#date normalisation expr() that allows writing SQL-like expressions inside spark
 transformed_customers_df = cleaned_customers_df.withColumn(
         'signup_date',#creates or replaces the column signup_date 
         coalesce(
@@ -126,8 +127,8 @@ transformed_returns_df = cleaned_returns_df.withColumn(
 transformed_order_items_df = cleaned_order_items_df
     
 #BOOLEAN COLUMN FOR -VE VALUES ON TOTAL AMOUNT COLUMN
-transformed_orders_df = transformed_orders_df.withColumn(
-        'is_negative_amount', #automatically auto assigns true/false base on the defined condition
+withBoolean_orders_df = transformed_orders_df.withColumn(
+        'is_negative_amount', #automatically auto assigns true/false based on the defined condition
         col("total_amount") < 0
     )
 
@@ -136,26 +137,29 @@ transformed_customers_df.show(truncate=False)
 transformed_order_items_df.show()
 transformed_orders_df.show()
 transformed_returns_df.show(truncate=False)
+withBoolean_orders_df.show()
 print('Completed printing the transformed data frames\n')
 
-joined_df1 = transformed_orders_df.join(transformed_customers_df, on='customer_id', how='inner') 
+#JOINS
 
-joined_df2 = joined_df1.join(transformed_order_items_df, on='order_id', how='inner')
+cust_orders_df = transformed_orders_df.join(transformed_customers_df, on='customer_id', how='inner') 
+
+cust_orders_items_df = cust_orders_df.join(transformed_order_items_df, on='order_id', how='inner')
 
 
 
-print('JOINS')
-joined_df1.show()
-joined_df2.show()
-print('End JOINS\n')
+print('JOINS start')
+cust_orders_df.show()
+cust_orders_items_df.show()
+print('JOINS End\n')
 
 #ORPHANED ITEMS
 orphaned_orders_df = transformed_order_items_df.join(transformed_orders_df, on='order_id', how='anti')
 
-print('Start Orphaned')
+print('Start Orphaned DFs')
 orphaned_orders_df.show()
 print(orphaned_orders_df.count())
-print('End Orphaned')
+print('End Orphaned DFs\n')
 
 
 derived_orders_df = transformed_orders_df.withColumn(
@@ -163,5 +167,41 @@ derived_orders_df = transformed_orders_df.withColumn(
     col('total_amount') * (1-col('discount_pct')/100)
 )
 
-print('Derived')
+print('Derived DFs Start')
 derived_orders_df.show()
+print(derived_orders_df.count())
+print('Derived DFs End\n')
+
+#AGGREGATIONS
+
+#using semi join to check the total number of unique customer orders thus proving existence of repeat orders
+count_unique_customer_orders_df = transformed_customers_df.join(transformed_orders_df, on='customer_id', how='semi')
+
+#calculate the lifetime net spend for each customer
+lifetime_net_spend_df = derived_orders_df.groupBy('customer_id').agg(sum('net_amount').alias('lifetime_net_spend'))
+
+#Join the customers df to the created lifetime net spend df and substitute nulls in the lifetime spend colmn with zero
+customer_lifetime_net_spend_df = transformed_customers_df.join(lifetime_net_spend_df, on='customer_id', how='left').fillna({'lifetime_net_spend': 0})
+
+#define a window partition to partition the customers by country and order by their lifetime spending
+windowPartition = Window.partitionBy('country').orderBy(col('lifetime_net_spend').desc())
+
+#create a df ranking the customers by spend per country 
+ranked_customer_lifetime_net_spend_df = customer_lifetime_net_spend_df.withColumn('customer_spend_rank_by_country', rank().over(windowPartition))
+
+#7 Day Rolling Order Count
+
+
+print('Start Window Functions')
+count_unique_customer_orders_df.show()
+print(count_unique_customer_orders_df.count())
+
+lifetime_net_spend_df.show()
+print(lifetime_net_spend_df.count())
+
+customer_lifetime_net_spend_df.show()
+print(customer_lifetime_net_spend_df.count())
+
+ranked_customer_lifetime_net_spend_df.show()
+print('End window functions')
+
